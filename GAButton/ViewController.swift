e77fdb8f18152ab71c84670ce27cd881a5612248
Derrick Ho <derrickho328@gmail.com>
2015-03-27 21:00:53 -0700

    - Finished GA Button!

--- GAButton/ViewController.swift
+++ GAButton/ViewController.swift
@@ -9,7 +9,8 @@
 import UIKit
 
 class MainView : UIView {
-    weak var delegate:UIViewController?
+    var coolButton:(UIButton)?
+    var hotButton:(GAButton)?
     
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -28,25 +29,36 @@ class MainView : UIView {
         coolButton.setTitle("What's Up?", forState: UIControlState.Normal)
         coolButton.backgroundColor = UIColor.blueColor()
         self.addSubview(coolButton)
-        coolButton.addTarget(delegate, action: "tappedButton:", forControlEvents: UIControlEvents.TouchUpInside)
+        self.coolButton = coolButton
         
+        var hotButton = GAButton(frame: CGRect(x: 0, y: 0, width: 200, height: 100))
+        hotButton.center = self.center
+        self.addSubview(hotButton)
+        self.hotButton = hotButton
     }
 }
 
 class ViewController: UIViewController {
     var mainView:(MainView?)
     var colorCount:(Int) = 0
+    var hotButton:(GAButton)?
+    var coldButton:(UIButton)?
     
     override func loadView() {
         super.loadView()
         self.view = MainView(frame: UIScreen.mainScreen().bounds)
         self.mainView = self.view as? MainView
-        self.mainView?.delegate = self
+        self.hotButton = (self.view as MainView).hotButton
+        self.coldButton = (self.view as MainView).coolButton
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        // Do any additional setup after loading the view, typically from a nib.  
+        self.coldButton?.addTarget(self, action: "tappedButton:", forControlEvents: UIControlEvents.TouchUpInside)
+        self.hotButton?.actionBlock = { (sender: GAButton) in
+            self.tappedGAButton(sender)
+        }
     }
 
     override func didReceiveMemoryWarning() {
@@ -59,6 +71,15 @@ class ViewController: UIViewController {
             self.mainView?.backgroundColor = UIColor.blueColor()
         } else {
             self.mainView?.backgroundColor = UIColor.redColor()
+        }
+        colorCount++
+    }
+    
+    func tappedGAButton(sender: GAButton) {
+        if colorCount % 2 == 0 {
+            self.mainView?.backgroundColor = UIColor.yellowColor()
+        } else {
+            self.mainView?.backgroundColor = UIColor.whiteColor()
         }
         colorCount++
     }
