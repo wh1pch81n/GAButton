ccf035bfda4933c109dc06e67b0ca6f65a745918
Derrick Ho <derrickho328@gmail.com>
2015-03-27 20:09:33 -0700

    - Added UIButton programmatically

--- GAButton/ViewController.swift
+++ GAButton/ViewController.swift
@@ -9,6 +9,8 @@
 import UIKit
 
 class MainView : UIView {
+    weak var delegate:UIViewController?
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
         self.setUpView()
@@ -21,14 +23,25 @@ class MainView : UIView {
     
     func setUpView() {
         self.backgroundColor = UIColor.redColor()
+        
+        var coolButton = UIButton(frame: CGRect(x: 0, y: 0, width: 200, height: 100))
+        coolButton.setTitle("What's Up?", forState: UIControlState.Normal)
+        coolButton.backgroundColor = UIColor.blueColor()
+        self.addSubview(coolButton)
+        coolButton.addTarget(delegate, action: "tappedButton:", forControlEvents: UIControlEvents.TouchUpInside)
+        
     }
 }
 
 class ViewController: UIViewController {
+    var mainView:(MainView?)
+    var colorCount:(Int) = 0
     
     override func loadView() {
         super.loadView()
         self.view = MainView(frame: UIScreen.mainScreen().bounds)
+        self.mainView = self.view as? MainView
+        self.mainView?.delegate = self
     }
     
     override func viewDidLoad() {
@@ -41,6 +54,13 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    func tappedButton(sender: UIButton!) {
+        if colorCount % 2 == 0 {
+            self.mainView?.backgroundColor = UIColor.blueColor()
+        } else {
+            self.mainView?.backgroundColor = UIColor.redColor()
+        }
+        colorCount++
+    }
 }
 
